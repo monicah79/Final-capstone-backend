9fa1ab4e4bd3f7651589a4a1dbba762712462824
monicah79 <monniewanjema@gmail.com>
2023-07-24 13:05:57 +0300

    Updated laptop reservations

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_22_162615) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_22_142735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "laptop_reservations", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "laptops", force: :cascade do |t|
     t.string "name"
