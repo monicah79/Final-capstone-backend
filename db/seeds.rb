7cfaed1ffd368ba09d5b9bcd1c9ff1e208259913
fuadmoin <fuadamoin@gmail.com>
2023-08-02 14:26:28 +0300

    Fix the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,27 @@
 # create a user
-user = User.create(email: 'user@example.com', password: 'password')
-user.save
+user = User.create!(email: 'user@example.com', password: 'password')
+
 
 # create a laptop
-laptop: {
+laptop= Laptop.create!(
+          user_id: user.id,
           name: 'MacBook Pro',
           price: 1299.99,
           cpu: 'Intel Core i7',
           memory: 16,
           picture: 'https',
           storage: 512
-        }
+)
 
 # create reservation
-reservation: {
-            laptop_id: laptop.id,
-            quantity: 1,
-            city: 'New York'
-          }
+reservation = Reservation.new
+reservation.user = user
+
+if reservation.save
+  laptop_reservation = LaptopReservation.create(
+    reservation_id: reservation.id,
+    laptop_id: laptop.id,
+    quantity: 1,
+    city: 'New York'
+  )
+end
